544cb4959963728613796b0c7a2e82830125b1e4
WesMcW <wesmcwhorter@gmail.com>
2020-09-22 13:29:09 -0500

    List stuff

---
+++ c# testroom/Assets/Scripts/Week3/GenericTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GenericTest<t> : MonoBehaviour
+{
+    t num1;
+    List<t> test = new List<t>();
+
+    public void PrintNum()
+    {
+        foreach(t i in test)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    public void AddNum(t num)
+    {
+        test.Add(num);
+    }
+
+    public void RemoveNum(int index)
+    {
+        test.RemoveAt(index);
+    }
+}
---
+++ c# testroom/Assets/Scripts/Week3/teet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teet : GenericTest<int>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PrintNum();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            AddNum(4);
+            AddNum(6);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RemoveNum(1);
+        }
+    }
+}
