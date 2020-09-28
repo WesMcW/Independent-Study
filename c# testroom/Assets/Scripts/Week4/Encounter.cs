e396baff18a51f8e8549281ab93ee00ad1542c6d
WesMcW <wesmcwhorter@gmail.com>
2020-09-28 12:01:53 -0500

    Started the scriptable objects

---
+++ c# testroom/Assets/Scripts/Week4/Encounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Encounter : MonoBehaviour
+{
+    public SceneBlueprint sceneInfo;
+    public Text titleText, descText, op1Text, op2Text, op3Text;
+    void Start()
+    {
+        SceneSetup();
+    }
+
+    void SceneSetup()
+    {
+        titleText.text = sceneInfo.title;
+        descText.text = sceneInfo.desc;
+        op1Text.text = sceneInfo.op1;
+        op2Text.text = sceneInfo.op2;
+        op3Text.text = sceneInfo.op3;
+    }
+}
---
+++ c# testroom/Assets/Scripts/Week4/SceneBlueprint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Scene", menuName = "ScriptableObjects/Scene", order = 1)]
+public class SceneBlueprint : ScriptableObject
+{
+    public string title, desc;
+    public string op1,op2,op3;
+
+}
