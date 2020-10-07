f21b719b7f2c7225dabcdc29995b5c42c00aab33
WesMcW <wesmcwhorter@gmail.com>
2020-10-07 11:30:50 -0500

    Delegate

---
+++ c# testroom/Assets/Scripts/Week5/DelegateTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DelegateTest : MonoBehaviour
+{
+    public delegate void SingleUse();
+    SingleUse single;
+    public static event SingleUse Single;
+
+    public int myNum;
+
+    private void Start()
+    {
+        Single += CountUp;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(Single != null)
+            {
+                CountUp();
+            }
+        }
+    }
+
+    public void CountUp()
+    {
+        myNum++;
+        print(myNum);
+    }
+}
